2c94b5d5971141c2394fe902ef136335769ddc56
Rishabh Jain <107767172+RishabhJain0721@users.noreply.github.com>
2024-06-20 01:34:23 +0530

    Added backend link

--- client/src/index.js
+++ client/src/index.js
@@ -7,8 +7,8 @@ import "./index.css";
 import App from "./App";
 import axios from "axios";
 
-// axios.defaults.baseURL = "http://192.168.43.156:3001"; // Replace with your API base URL
-axios.defaults.baseURL = "http://localhost:3001"; // Replace with your API base URL
+axios.defaults.baseURL = "https://opinod.onrender.com"; // Replace with your API base URL
+// axios.defaults.baseURL = "http://localhost:3001"; // Replace with your API base URL
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
