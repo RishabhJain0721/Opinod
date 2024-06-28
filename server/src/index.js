c042a535f74d63d6de9b81bff70f94e82d17d422
Rishabh <rishujain0721@gmail.com>
2024-06-28 23:40:59 +0530

    Custom headers added

--- server/src/index.js
+++ server/src/index.js
@@ -8,6 +8,17 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+// Middleware to add a custom header to all responses
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 const MONGO_URI = process.env.MONGODB_CONNECTION_STRING;
 const PORT = process.env.PORT || 3001;
 
@@ -17,7 +28,7 @@ mongoose
   .then(() => {
     console.log("Connected to MongoDB");
     app.listen(PORT, () => {
-      console.log("Server is running on port 3001");
+      console.log(`Server is running on port ${PORT}`);
     });
   })
   .catch((error) => {
